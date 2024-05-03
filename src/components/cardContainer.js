df2c41f7fc1e9fe0d132849cdcc3f5d84cf1c6d4
param <param11650@gmail.com>
2024-05-03 18:39:55 +0530

    feat: Remove Console

--- src/components/cardContainer.js
+++ src/components/cardContainer.js
@@ -4,9 +4,6 @@ import Card from './card';
 import CardLoader from './cardLoader';
 const CardContainer = ({cardsArray,isLoading}) => {
   let index=0
-  // const cardsArray = useSelector(state => state?.data?.cardsArray);
-  // const filteredJobs = cardsArray.filter(job => job.jobRole === 'frontend');
-  console.log(cardsArray.length)
 
   return (
     <div className='card_container_div'>
